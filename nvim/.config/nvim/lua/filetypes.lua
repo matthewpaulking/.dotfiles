2ca68b891faa5f5348d7785d8e06a5beaf3ee135
Matthew King <matthewpaulking@gmail.com>
2024-10-04 17:14:59 -0700

    blade treesitter stuff

--- nvim/.config/nvim/lua/filetypes.lua
+++ nvim/.config/nvim/lua/filetypes.lua
@@ -2,7 +2,7 @@ vim.api.nvim_exec(
     [[
         augroup framework_filetype_settings
             autocmd!
-            autocmd BufRead,BufNewFile *.blade.php setlocal commentstring={{--\ %s\ --}} filetype=html
+            autocmd BufRead,BufNewFile *.blade.php setlocal commentstring={{--\ %s\ --}} filetype=blade
             autocmd BufRead,BufNewFile *.njk setlocal commentstring={{--\ %s\ --}} filetype=njk
             " autocmd BufRead,BufNewFile *.antlers.html setlocal commentstring={{#\ %s\ #}} filetype=html
             autocmd FileType markdown setlocal spell
