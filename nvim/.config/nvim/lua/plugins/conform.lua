ad8280ec693c0926990193c48356225038689527
Matthew King <matthewpaulking@gmail.com>
2024-10-09 09:58:10 -0700

    add vue and php formatters to conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,8 @@ return {
             sh = { 'shfmt' },
             lua = { 'stylua' },
             javascript = { 'prettierd', 'prettier', stop_after_first = true },
+            vue = { 'prettierd', 'prettier', stop_after_first = true },
+            php = { 'pint', 'php-cs-fixer', stop_after_first = true },
         },
         format_on_save = {
             timeout_ms = 500,
