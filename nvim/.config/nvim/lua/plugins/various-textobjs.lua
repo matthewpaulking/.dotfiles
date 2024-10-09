5b0d54b51d647bb8f86d0736d0b6576877e516ae
Matthew King <matthewpaulking@gmail.com>
2024-10-09 10:49:17 -0700

    only use subword feature of various-textobjs

--- nvim/.config/nvim/lua/plugins/various-textobjs.lua
+++ nvim/.config/nvim/lua/plugins/various-textobjs.lua
@@ -1,5 +1,9 @@
 return {
     'chrisgrieser/nvim-various-textobjs',
     event = 'UIEnter',
-    opts = { useDefaultKeymaps = true },
+    opts = { useDefaultKeymaps = false },
+    keys = {
+        { 'iS', '<cmd>lua require("various-textobjs").subword("inner")<CR>', mode = { 'o', 'x' } },
+        { 'aS', '<cmd>lua require("various-textobjs").subword("outer")<CR>', mode = { 'o', 'x' } },
+    },
 }
