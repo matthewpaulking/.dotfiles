3c0a8938dda03a466de342dbdc87e63c7f06977e
Matthew King <matthewpaulking@gmail.com>
2024-10-16 12:00:45 -0700

    add custom bind for elements in tshjkl

--- nvim/.config/nvim/lua/plugins/tshjkl.lua
+++ nvim/.config/nvim/lua/plugins/tshjkl.lua
@@ -1,9 +1,35 @@
 return {
     'gsuuon/tshjkl.nvim',
-    config = true,
     opts = {
         keymaps = {
             toggle = '<leader><leader>j',
         },
+        binds = function(bind, tshjkl)
+            bind('<Esc>', function()
+                tshjkl.exit(true)
+            end)
+
+            bind('q', function()
+                tshjkl.exit(true)
+            end)
+
+            bind('t', function()
+                print(tshjkl.current_node():type())
+            end)
+            bind('e', function()
+                local node = tshjkl.current_node()
+
+                while node do
+                    local type = node:type()
+
+                    if type:match('element') then -- declaration or definition
+                        tshjkl.set_node(node)
+                        return
+                    end
+
+                    node = node:parent()
+                end
+            end)
+        end,
     },
 }
