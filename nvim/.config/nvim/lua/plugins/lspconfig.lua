a2a77c11815abae3b8e045efa30a645380ed91bc
Matthew King <matthewpaulking@gmail.com>
2024-10-15 18:03:38 -0700

    get location of vue-langauage-server from mason to correctly install @vue/typescript-plugin

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -48,12 +48,16 @@ return {
             capabilities = capabilities,
         })
 
+        local mason_registry = require('mason-registry')
+        local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path()
+            .. '/node_modules/@vue/language-server'
+
         require('lspconfig').ts_ls.setup({
             init_options = {
                 plugins = {
                     {
                         name = '@vue/typescript-plugin',
-                        location = '/usr/local/lib/node_modules/@vue/typescript-plugin',
+                        location = vue_language_server_path,
                         languages = { 'javascript', 'typescript', 'vue' },
                     },
                 },
