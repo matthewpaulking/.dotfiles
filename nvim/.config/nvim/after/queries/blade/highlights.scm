584a20b9003d059b68c1b651eb4864ae1cbaf5ae
Matthew King <matthewpaulking@gmail.com>
2024-10-17 00:12:55 -0700

    fix blade highlights and injections

--- nvim/.config/nvim/after/queries/blade/highlights.scm
+++ nvim/.config/nvim/after/queries/blade/highlights.scm
@@ -2,8 +2,12 @@
 (directive_start) @function
 (directive_end) @function
 (comment) @comment
-((parameter) @include (#set! "priority" 110))
-((php_only) @include (#set! "priority" 110))
+
+;INFO: no idea why commenting out these lines fixes the highlighting
+;https://github.com/EmranMR/tree-sitter-blade/discussions/19#discussioncomment-8768127
+;((parameter) @include (#set! "priority" 110))
+;((php_only) @include (#set! "priority" 110))
+
 ((bracket_start) @function (#set! "priority" 120))
 ((bracket_end) @function (#set! "priority" 120))
 (keyword) @function
