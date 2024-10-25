6c0401e4f4ccc6438395d3303c2fecae73cb5e9f
Matthew King <matthewpaulking@gmail.com>
2024-10-24 23:27:50 -0700

    remove unused function from conform config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,20 +1,6 @@
 return {
     'stevearc/conform.nvim',
     config = function()
-        ---@param bufnr integer
-        ---@param ... string
-        ---@return string
-        local function first(bufnr, ...)
-            local conform = require('conform')
-            for i = 1, select('#', ...) do
-                local formatter = select(i, ...)
-                if conform.get_formatter_info(formatter, bufnr).available then
-                    return formatter
-                end
-            end
-            return select(1, ...)
-        end
-
         require('conform').setup({
             formatters_by_ft = {
                 sh = { 'shfmt' },
