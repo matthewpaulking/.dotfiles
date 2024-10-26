aa55dc3843931e8fb5371247898639ee2f3f4e96
Matthew King <matthewpaulking@gmail.com>
2024-10-25 21:46:17 -0700

    markview

---
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons',
+    },
+    keys = {
+        { '<leader><leader>v', '<cmd>Markview<cr>', desc = 'Markview' },
+    },
+}
